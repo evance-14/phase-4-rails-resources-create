2b2be30f91c3a1c72b611996ddfbfa52073e57da
evance-14 <kiropevans097@gmail.com>
2023-03-15 12:39:51 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
+  # # GET /birds
   def index
     birds = Bird.all
     render json: birds
@@ -14,6 +14,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  #POST /bird
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
